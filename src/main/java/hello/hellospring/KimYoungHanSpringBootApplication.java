5b4d120be86df10aaeadf910dc707d4905d87c9c
chae-hwa <ihj0519@naver.com>
2022-08-15 16:48:42 +0900

    세팅

---
+++ src/main/java/hello/hellospring/KimYoungHanSpringBootApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimYoungHanSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimYoungHanSpringBootApplication.class, args);
+    }
+
+}
